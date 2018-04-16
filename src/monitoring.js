8014a163b4d80ae2a200a007542cce818ed9153d
Quentin VERLHAC <quentin.verlhac@student.ecp.fr>
2018-04-16 16:19:09 +0200

    feat: add socket connection

--- src/monitoring.js
+++ src/monitoring.js
@@ -3,13 +3,17 @@ const axios = require('axios');
 const io = require('socket.io-client');
 const config = require('../config.json');
 
+let socket;
+
 function monitor() {
+  socket = io(config.urlBack);
   axios.get(`${config.urlBack}/monitoring`);
   console.log('Puppy is monitoring !');
 }
 
 function stop() {
   axios.delete(`${config.urlBack}/monitoring`);
+  socket.disconnect();
   console.log('Puppy stopped monitoring.');
 }
 
