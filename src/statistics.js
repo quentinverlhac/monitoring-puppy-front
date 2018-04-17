19e86b754d83c8d39e85161f156228c736c8aca9
Quentin VERLHAC <quentin.verlhac@student.ecp.fr>
2018-04-17 13:06:03 +0200

    refactor: display statistic

--- src/statistics.js
+++ src/statistics.js
@@ -2,9 +2,8 @@
 const axios = require('axios');
 const config = require('../config.json');
 
-async function displayStatistics(websiteName, duration) {
-  const response = await axios.get(`${config.urlBack}/statistic/${websiteName}/${duration}`);
-  console.log(response.data);
+async function displayStatistics(statistics) {
+  console.log(statistics);
 }
 
 module.exports = displayStatistics;
