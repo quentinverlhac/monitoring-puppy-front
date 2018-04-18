e25b71b13957215ef914b29bb6e36f4ba2a336af
Quentin VERLHAC <quentin.verlhac@student.ecp.fr>
2018-04-18 14:04:19 +0200

    refactor: config.json

--- src/website.js
+++ src/website.js
@@ -6,7 +6,7 @@ const config = require('../config.json');
 async function addWebsite(website, url, check_interval) {
   // Send website creation request to back end
   const response = await axios.post(
-    `${config.urlBack}/website`,
+    `${config.urlBack}/api/website`,
     {
       name: website,
       url,
@@ -20,7 +20,7 @@ async function addWebsite(website, url, check_interval) {
 // The function which handle the list command
 async function listWebsites() {
   // Retrieve websites from back end
-  const response = await axios.get(`${config.urlBack}/website`);
+  const response = await axios.get(`${config.urlBack}/api/website`);
   // Display website
   console.log('Puppy has registered the following websites:');
   console.log('-------------------------------------');
@@ -35,7 +35,7 @@ async function listWebsites() {
 async function updateWebsite(website, options) {
   // Send website update request to back end
   const response = await axios.put(
-    `${config.urlBack}/website/${website}`,
+    `${config.urlBack}/api/website/${website}`,
     {
       name: options.name,
       url: options.url,
@@ -49,7 +49,7 @@ async function updateWebsite(website, options) {
 // The function which handle the remove command
 async function removeWebsite(website) {
   // Send website creation request to back end
-  const response = await axios.delete(`${config.urlBack}/website/${website}`);
+  const response = await axios.delete(`${config.urlBack}/api/website/${website}`);
   // Display a confirmation
   console.log(`${response.data.name} (${response.data.url}) has been removed`);
 }
