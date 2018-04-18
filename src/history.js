ef8a40b58621233983fa80ac97c619e43e686959
Quentin VERLHAC <quentin.verlhac@student.ecp.fr>
2018-04-18 22:58:42 +0200

    feat: add history error management

--- src/history.js
+++ src/history.js
@@ -2,10 +2,17 @@
 const axios = require('axios');
 const config = require('../config.json');
 const displayHistory = require('./displayHistory');
+const handleError = require('./errorHandler');
 
 async function getHistory() {
-  const response = await axios.get(`${config.urlBack}/api/history/0`);
-  displayHistory(response.data);
+  try {
+    // Send get history request to back end
+    const response = await axios.get(`${config.urlBack}/api/history/0`);
+    displayHistory(response.data);
+  } catch (err) {
+  // The request wasn't successful
+    handleError(err);
+  }
 }
 
 module.exports = getHistory;
