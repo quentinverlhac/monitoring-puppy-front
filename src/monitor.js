0b8b2e27e8be948925d15aa54fdb4f54349e2e3a
Quentin VERLHAC <quentin.verlhac@student.ecp.fr>
2018-04-18 22:58:31 +0200

    refactor: monitoring file

--- src/monitor.js
+++ src/monitor.js
@@ -1,20 +1,26 @@
 // Import node modules
 const config = require('../config.json');
-const displayStatistics = require('./statistics');
+const displayStatistics = require('./displayStatistics');
 const displayAlert = require('./displayAlert');
 const io = require('socket.io-client');
+const handleError = require('./errorHandler');
 
 let socket;
 
 function monitor() {
-  socket = io(config.urlBack);
-  socket.on('statistics', (statistics) => {
-    displayStatistics(statistics);
-  });
-  socket.on('alert', (alert) => {
-    displayAlert(alert);
-  });
-  console.log('Puppy is monitoring, displaying statistics and sending alerts !');
+  try {
+    socket = io(config.urlBack);
+    socket.on('statistics', (statistics) => {
+      displayStatistics(statistics);
+    });
+    socket.on('alert', (alert) => {
+      displayAlert(alert);
+    });
+    console.log('Puppy is monitoring, displaying statistics and sending alerts !');
+  } catch (err) {
+  // The request wasn't successful
+    handleError(err);
+  }
 }
 
 module.exports = monitor;
