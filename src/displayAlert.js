e06f2cb4647f222a3facda69de58055fea7881b0
Quentin VERLHAC <quentin.verlhac@student.ecp.fr>
2018-04-18 22:43:44 +0200

    feat: display alert

--- src/displayAlert.js
+++ src/displayAlert.js
@@ -1,5 +1,5 @@
 async function displayAlert(alert) {
-  console.log(alert);
+  console.log(`${alert.website.name} (${alert.website.url}) went ${alert.status} with an availability of ${alert.availability} on ${new Date(alert.dateTimestamp)}`);
 }
 
 module.exports = displayAlert;
