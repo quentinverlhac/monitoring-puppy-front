2898fb56419560a2b0f16567663ff1e16c01d252
Quentin VERLHAC <quentin.verlhac@student.ecp.fr>
2018-04-18 16:09:05 +0200

    doc: add command doc

--- main.js
+++ main.js
@@ -13,16 +13,18 @@ const getHistory = require('./src/history');
 // Define metadata of the application
 program
   .version('0.1.0')
-  .description('Puppy is a website monitoring application.\n  You can add several websites to monitor and run the monitoring to display statistics and alerts.\n  It is also possible to keep on checking websites without displaying messages in the console.');
+  .description('Puppy is a website monitoring application.\n  You can add several websites to monitor and run the monitoring to display statistics and alerts.\n  It is also possible to keep on checking websites without displaying messages in the console.\n');
 
 // Add website command
 program
   .command('add <website> <url> <check_interval>')
+  .description('\nAdd a website to monitoring\n  <website> [string] is the name of the website\n  <url> [string] is the url of the website\n  <check_interval> [number] is the time interval between each website check\n')
   .action(addWebsite);
 
 // List monitored websites command
 program
   .command('list')
+  .description('\nList all monitored websites\n')
   .action(listWebsites);
 
 // Update website command
@@ -31,31 +33,37 @@ program
   .option('-n, --name [string]', 'the new website name')
   .option('-u, --url [string]', 'the new website url')
   .option('-i, --interval [number]', 'the new check interval')
+  .description('\nUpdate one of several fields of the website given by its name <website>\n  -n --name [string] the new name of the website\n  -u --url [string] the new url of the website\n  -i --interval [number] the new time interval between website checks\n')
   .action(updateWebsite);
 
 // Add website command
 program
   .command('remove <website>')
+  .description('\nRemove the website given by its name <website>\n')
   .action(removeWebsite);
 
 // Monitoring command
 program
   .command('monitor')
+  .description('\nStart the checking of all the websites on the remote server\n  /!\\ CAUTION /!\\ the checking will not stop until "puppy stop" command is run, even if this shell is closed\n')
   .action(monitor);
 
 // Stop monitoring command
 program
   .command('stop')
+  .description('\nStop the checking of all websites\n')
   .action(stop);
 
 // Run command
 program
   .command('run')
+  .description('\nStart the monitoring of websites\n  Regularly, websites are checked, statistics are displayed and alert are prompted if there is an emergency\n  End the monitoring by pressing CTRL+C in this shell. This will stop the checking.\n')
   .action(run);
 
 // History command
 program
   .command('history')
+  .description('\nGet the history of past alerts\n')
   .action(getHistory);
 
 program
