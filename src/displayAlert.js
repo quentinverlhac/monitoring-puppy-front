6be454b8b2523cbe147458094fef5164287afcec
Quentin VERLHAC <quentin.verlhac@student.ecp.fr>
2018-04-21 11:30:37 +0200

    refactor: display alerts

--- src/displayAlert.js
+++ src/displayAlert.js
@@ -1,5 +1,17 @@
 async function displayAlert(alert) {
+  // The following lines are used to display readable statistics
+  // Empty lines are used to add space so alerts can be easily identified
+  console.log('');
+  console.log('/!\\ ALERT /!\\');
+  console.log('');
+  // console.group() add indentation
+  console.group();
   console.log(`${alert.website.name} (${alert.website.url}) went ${alert.status} with an availability of ${alert.availability} on ${new Date(alert.dateTimestamp)}`);
+  // console.groupEnd() remove indentation
+  console.groupEnd();
+  console.log('');
+  console.log('-- END --');
+  console.log('');
 }
 
 module.exports = displayAlert;
