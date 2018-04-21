89b511677f7a01922cc21ebb8bb57eb489cd449c
Quentin VERLHAC <quentin.verlhac@student.ecp.fr>
2018-04-21 13:19:29 +0200

    refactor: check routes

--- src/check.js
+++ src/check.js
@@ -3,12 +3,12 @@ const axios = require('axios');
 const config = require('../config.json');
 
 function check() {
-  axios.get(`${config.urlBack}/api/monitoring`);
+  axios.get(`${config.urlBack}/api/monitoring/run`);
   console.log('Puppy now checks websites');
 }
 
 function stop() {
-  axios.delete(`${config.urlBack}/api/monitoring`);
+  axios.get(`${config.urlBack}/api/monitoring/stop`);
   console.log('Puppy doesn\'t check anymore');
 }
 
