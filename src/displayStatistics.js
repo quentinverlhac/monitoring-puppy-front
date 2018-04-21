3fbd24b2afa54e9a9ca25d57aadcec743fbb493c
Quentin VERLHAC <quentin.verlhac@student.ecp.fr>
2018-04-21 16:00:35 +0200

    refactor: display statistics

--- src/displayStatistics.js
+++ src/displayStatistics.js
@@ -1,23 +1,18 @@
 async function displayStatistics(statistics, blessedBox) {
   // The following lines are used to display readable statistics
-  blessedBox.pushLine(`${new Date(statistics.date)} - Statistics of ${statistics.websiteName} for the last ${statistics.duration / 1000} seconds:`);
-  // console.group() add indentation
-  // console.group();
+  blessedBox.pushLine(`${new Date(statistics.date)}`);
+  blessedBox.pushLine(`Statistics of ${statistics.websiteName} for the last ${statistics.duration / 1000} seconds:`);
   blessedBox.pushLine(`Average availability: ${statistics.availability}`);
   blessedBox.pushLine(`Maximum response time: ${statistics.maxResponseTime} ms`);
   blessedBox.pushLine(`Average response time: ${statistics.averageResponseTime} ms`);
   blessedBox.pushLine('Response status codes count:');
-  // console.group();
   Object.keys(statistics.responseCodes).map((code) => {
     if (code !== 'null') {
-      blessedBox.pushLine(`${code}: ${statistics.responseCodes[code]} occurences`);
+      blessedBox.pushLine(`{right}${code}: ${statistics.responseCodes[code]} occurences{/right}`);
     } else {
-      blessedBox.pushLine(`Without response: ${statistics.responseCodes[code]} occurences`);
+      blessedBox.pushLine(`{right}Without response: ${statistics.responseCodes[code]} occurences{/right}`);
     }
   });
-  // console.groupEnd() remove indentation
-  // console.groupEnd();
-  // console.groupEnd();
   // A last line to add space between different websites statistics
   blessedBox.pushLine('');
 }
