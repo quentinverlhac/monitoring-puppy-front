6cd21fbcb10acb1556d4ee287d7adf9a76dc7e58
Quentin VERLHAC <quentin.verlhac@student.ecp.fr>
2018-04-21 16:58:58 +0200

    refactor: logBox config

--- src/blessedComponents/logBox.js
+++ src/blessedComponents/logBox.js
@@ -3,26 +3,25 @@ function setUpLogBox(blessed) {
   const logBox = blessed.log({
     top: '0%',
     left: '0%',
-    valign: 'top',
-    align: 'left',
     width: '50%',
     height: '100%',
-    content: 'Puppy will display monitoring statistics here\n',
+    style: {
+      fg: 'white',
+      bg: 'black',
+    },
+    border: {
+      type: 'line',
+    },
     tags: true,
+    content: 'Puppy will display monitoring statistics here\n',
     scrollable: true,
     scrollbar: {
       ch: ' ',
       inverse: true,
     },
+    mouse: true,
     keys: true,
-    scrollOnInput: false,
-    border: {
-      type: 'line',
-    },
-    style: {
-      fg: 'white',
-      bg: 'black',
-    },
+    vi: true,
   });
   return logBox;
 }
