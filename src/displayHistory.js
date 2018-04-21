9fdd4e7b446fa405001d3fad5e6bd8988d14fa1f
Quentin VERLHAC <quentin.verlhac@student.ecp.fr>
2018-04-21 17:33:01 +0200

    refactor: history date

--- src/displayHistory.js
+++ src/displayHistory.js
@@ -1,7 +1,8 @@
 async function displayHistory(history) {
   history.map((alert) => {
+    const date = (new Date(alert.dateTimestamp)).toLocaleString();
     // The following lines are used to display readable statistics
-    console.log(`${alert.website.name} (${alert.website.url}) went ${alert.status} with an availability of ${alert.availability} on ${new Date(alert.dateTimestamp)}`);
+    console.log(`${date} - ${alert.website.name} (${alert.website.url}) went ${alert.status} with an availability of ${alert.availability}`);
     // Empty lines are used to add space so alerts can be easily identified
     console.log('');
   });
