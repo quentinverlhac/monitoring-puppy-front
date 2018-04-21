d65bdf1e6ee56e18814ea1a93c182850592bae60
Quentin VERLHAC <quentin.verlhac@student.ecp.fr>
2018-04-21 16:00:43 +0200

    refactor: displayAlert

--- src/displayAlert.js
+++ src/displayAlert.js
@@ -1,16 +1,14 @@
 async function displayAlert(alert, blessedBox) {
-  // The following lines are used to display readable statistics
-  // Empty lines are used to add space so alerts can be easily identified
-  blessedBox.pushLine('');
+  let statusTag = '{red-fg}DOWN{/}';
+  if (alert.status === 'up') {
+    statusTag = '{green-fg}UP{/}';
+  }
+  // The following lines are used to display readable alerts
   blessedBox.pushLine('/!\\ ALERT /!\\');
-  blessedBox.pushLine('');
-  // console.group() add indentation
-  // console.group();
-  blessedBox.pushLine(`${alert.website.name} (${alert.website.url}) went ${alert.status} with an availability of ${alert.availability} on ${new Date(alert.dateTimestamp)}`);
-  // console.groupEnd() remove indentation
-  // console.groupEnd();
-  blessedBox.pushLine('');
-  blessedBox.pushLine('-- END --');
+  blessedBox.pushLine(`${alert.website.name} (${alert.website.url}) is ${statusTag}`);
+  blessedBox.pushLine(`Date: ${new Date(alert.dateTimestamp)}`);
+  blessedBox.pushLine(`Availability: ${alert.availability} (over the last 2 minutes)`);
+  // Empty lines are used to add space so alerts can be easily identified
   blessedBox.pushLine('');
 }
 
