3f37a99e09144aaa4a8d40cc6c9a9691e06858cb
Quentin VERLHAC <quentin.verlhac@student.ecp.fr>
2018-04-21 16:32:02 +0200

    refactor: display alert

--- src/displayAlert.js
+++ src/displayAlert.js
@@ -1,13 +1,12 @@
 async function displayAlert(alert, blessedBox) {
+  const date = (new Date(alert.dateTimestamp)).toLocaleString();
   let statusTag = '{red-fg}DOWN{/}';
   if (alert.status === 'up') {
     statusTag = '{green-fg}UP{/}';
   }
   // The following lines are used to display readable alerts
-  blessedBox.pushLine('/!\\ ALERT /!\\');
-  blessedBox.pushLine(`${alert.website.name} (${alert.website.url}) is ${statusTag}`);
-  blessedBox.pushLine(`Date: ${new Date(alert.dateTimestamp)}`);
-  blessedBox.pushLine(`Availability: ${alert.availability} (over the last 2 minutes)`);
+  blessedBox.log(`{blue-fg}${date}{/} - {yellow-fg}${alert.website.name}{/} (${alert.website.url}) is ${statusTag}`);
+  blessedBox.log(`Availability: ${alert.availability} (over the last 2 minutes)`);
   // Empty lines are used to add space so alerts can be easily identified
   blessedBox.pushLine('');
 }
