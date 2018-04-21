0c9687a13ea9ee021c2323309689f426475e7acc
Quentin VERLHAC <quentin.verlhac@student.ecp.fr>
2018-04-21 16:04:35 +0200

    refactor: display history

--- src/displayHistory.js
+++ src/displayHistory.js
@@ -1,8 +1,9 @@
-const displayAlert = require('./displayAlert');
-
 async function displayHistory(history) {
   history.map((alert) => {
-    displayAlert(alert);
+    // The following lines are used to display readable statistics
+    console.log(`${alert.website.name} (${alert.website.url}) went ${alert.status} with an availability of ${alert.availability} on ${new Date(alert.dateTimestamp)}`);
+    // Empty lines are used to add space so alerts can be easily identified
+    console.log('');
   });
 }
 
