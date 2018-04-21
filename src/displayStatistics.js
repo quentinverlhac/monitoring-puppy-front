5a8ebef7a262a3da7c45630f944035d752b8065d
Quentin VERLHAC <quentin.verlhac@student.ecp.fr>
2018-04-21 15:08:04 +0200

    feat: display stats ins blessed box

--- src/displayStatistics.js
+++ src/displayStatistics.js
@@ -1,25 +1,25 @@
-async function displayStatistics(statistics) {
+async function displayStatistics(statistics, blessedBox) {
   // The following lines are used to display readable statistics
-  console.log(`${new Date(statistics.date)} - Statistics of ${statistics.websiteName} for the last ${statistics.duration / 1000} seconds:`);
+  blessedBox.pushLine(`${new Date(statistics.date)} - Statistics of ${statistics.websiteName} for the last ${statistics.duration / 1000} seconds:`);
   // console.group() add indentation
-  console.group();
-  console.log(`Average availability: ${statistics.availability}`);
-  console.log(`Maximum response time: ${statistics.maxResponseTime} ms`);
-  console.log(`Average response time: ${statistics.averageResponseTime} ms`);
-  console.log('Response status codes count:');
-  console.group();
+  // console.group();
+  blessedBox.pushLine(`Average availability: ${statistics.availability}`);
+  blessedBox.pushLine(`Maximum response time: ${statistics.maxResponseTime} ms`);
+  blessedBox.pushLine(`Average response time: ${statistics.averageResponseTime} ms`);
+  blessedBox.pushLine('Response status codes count:');
+  // console.group();
   Object.keys(statistics.responseCodes).map((code) => {
     if (code !== 'null') {
-      console.log(`${code}: ${statistics.responseCodes[code]} occurences`);
+      blessedBox.pushLine(`${code}: ${statistics.responseCodes[code]} occurences`);
     } else {
-      console.log(`Without response: ${statistics.responseCodes[code]} occurences`);
+      blessedBox.pushLine(`Without response: ${statistics.responseCodes[code]} occurences`);
     }
   });
   // console.groupEnd() remove indentation
-  console.groupEnd();
-  console.groupEnd();
+  // console.groupEnd();
+  // console.groupEnd();
   // A last line to add space between different websites statistics
-  console.log('');
+  blessedBox.pushLine('');
 }
 
 module.exports = displayStatistics;
