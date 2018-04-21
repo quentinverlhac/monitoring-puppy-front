f3510719921db7985ce906e4e53bc618fd73c3fc
Quentin VERLHAC <quentin.verlhac@student.ecp.fr>
2018-04-21 16:29:14 +0200

    refactor: display statistics

--- src/displayStatistics.js
+++ src/displayStatistics.js
@@ -1,16 +1,16 @@
 async function displayStatistics(statistics, blessedBox) {
   // The following lines are used to display readable statistics
-  blessedBox.pushLine(`${new Date(statistics.date)}`);
-  blessedBox.pushLine(`Statistics of ${statistics.websiteName} for the last ${statistics.duration / 1000} seconds:`);
-  blessedBox.pushLine(`Average availability: ${statistics.availability}`);
-  blessedBox.pushLine(`Maximum response time: ${statistics.maxResponseTime} ms`);
-  blessedBox.pushLine(`Average response time: ${statistics.averageResponseTime} ms`);
-  blessedBox.pushLine('Response status codes count:');
+  const date = (new Date(statistics.date)).toLocaleString();
+  blessedBox.log(`{blue-fg}${date}{/} - {yellow-fg}${statistics.websiteName}{/} statistics for the last ${statistics.duration / 1000} seconds`);
+  blessedBox.log(`Average availability: ${statistics.availability}`);
+  blessedBox.log(`Maximum response time: ${statistics.maxResponseTime} ms`);
+  blessedBox.log(`Average response time: ${statistics.averageResponseTime} ms`);
+  blessedBox.log('Response status codes count:');
   Object.keys(statistics.responseCodes).map((code) => {
     if (code !== 'null') {
-      blessedBox.pushLine(`{right}${code}: ${statistics.responseCodes[code]} occurences{/right}`);
+      blessedBox.log(`  ${code}: ${statistics.responseCodes[code]} occurences`);
     } else {
-      blessedBox.pushLine(`{right}Without response: ${statistics.responseCodes[code]} occurences{/right}`);
+      blessedBox.log(`  Without response: ${statistics.responseCodes[code]} occurences`);
     }
   });
   // A last line to add space between different websites statistics
