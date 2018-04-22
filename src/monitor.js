f3385ee26eba34e42947cc139122fe4067275ddd
Quentin VERLHAC <quentin.verlhac@student.ecp.fr>
2018-04-22 11:20:37 +0200

    doc: add monitor function

--- src/monitor.js
+++ src/monitor.js
@@ -10,10 +10,16 @@ const setUpAlertBox = require('./blessedComponents/alertBox');
 
 let socket;
 
-
+// This function tells the back to ping websites, to compute statistics and to send alerts.
+// The function sets up two boxes in terminal, using blessed module.
+// There is one box for statistics and the other for alerts.
+// Then the funciton opens a socket (bilateral connection) with the back end.
+// When the socket is opened, the back starts to ping websites.
+// The back sends statistics and alerts through the socket.
+// Once received by the front, they are displayed in the boxes using displayers functions.
 function monitor() {
   try {
-    // Create a screen object
+    // Create a screen object using blessed
     const screen = blessed.screen({
       smartCSR: true,
       title: 'monitoring-puppy',
@@ -27,7 +33,7 @@ function monitor() {
     screen.append(logBox);
     screen.append(alertBox);
 
-    // Quit on Escape, q, or Control-C.
+    // Capture used inputs: quit on Escape, q, or Control-C.
     screen.key(['escape', 'q', 'C-c'], (ch, key) => process.exit(0));
 
     // Render the screen
@@ -35,15 +41,16 @@ function monitor() {
 
     // Set up socket connection to back
     socket = io(config.urlBack);
+    // When the socket receive statistics, display them in the statistics box using statistics displayer
     socket.on('statistics', (statistics) => {
       displayStatistics(statistics, logBox);
       screen.render();
     });
+    // When the socket receive alerts, display them in the alert box using alert displayer
     socket.on('alert', (alert) => {
       displayAlert(alert, alertBox);
       screen.render();
     });
-    console.log('Puppy is monitoring, displaying statistics and sending alerts !');
   } catch (err) {
   // The request wasn't successful
     handleError(err);
