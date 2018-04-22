4c1fd3ed4bca2c2829e60e7fcbebb3d105862bc8
Quentin VERLHAC <quentin.verlhac@student.ecp.fr>
2018-04-22 11:09:54 +0200

    doc: displayHistory

--- src/displayHistory.js
+++ src/displayHistory.js
@@ -1,5 +1,8 @@
+// This function is responsible for displaying a human readable history in terminal
 async function displayHistory(history) {
+  // Apply the following pattern for each alert in history array
   history.map((alert) => {
+    // Refactor the date to be human readable
     const date = (new Date(alert.dateTimestamp)).toLocaleString();
     // The following lines are used to display readable statistics
     console.log(`${date} - ${alert.website.name} (${alert.website.url}) went ${alert.status} with an availability of ${alert.availability}`);
@@ -8,5 +11,6 @@ async function displayHistory(history) {
   });
 }
 
+// Export function
 module.exports = displayHistory;
 
