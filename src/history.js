82f7b81c9bfe0216970edf78f047e4116f40ead2
Quentin VERLHAC <quentin.verlhac@student.ecp.fr>
2018-04-22 11:14:15 +0200

    doc: history

--- src/history.js
+++ src/history.js
@@ -4,10 +4,13 @@ const config = require('../config.json');
 const displayHistory = require('./displayHistory');
 const handleError = require('./errorHandler');
 
+// This function calls the get history route of the back
+// It gets all the alerts and display them using displayHistory function
 async function getHistory() {
   try {
     // Send get history request to back end
     const response = await axios.get(`${config.urlBack}/api/alert/0`);
+    // Display history using displayHistory function
     displayHistory(response.data);
   } catch (err) {
   // The request wasn't successful
