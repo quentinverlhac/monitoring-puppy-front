2a62d739cb59f8649e90aa568bb13ece764c1b4e
Quentin VERLHAC <quentin.verlhac@student.ecp.fr>
2018-04-22 11:06:32 +0200

    doc: check

--- src/check.js
+++ src/check.js
@@ -2,16 +2,21 @@
 const axios = require('axios');
 const config = require('../config.json');
 
+// This function calls the run monitoring route of the back
+// It starts the regular ping of website in background
 function check() {
   axios.get(`${config.urlBack}/api/monitoring/run`);
   console.log('Puppy now checks websites');
 }
 
+// This function calls the stop monitoring route of the back
+// It stops the regular ping of website in background
 function stop() {
   axios.get(`${config.urlBack}/api/monitoring/stop`);
   console.log('Puppy doesn\'t check anymore');
 }
 
+// Export functions
 module.exports = {
   check,
   stop,
